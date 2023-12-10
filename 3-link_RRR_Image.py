1f3f0a42ca5d2ce045cb1692e65fd18c968fd7d3
sdds3441 <khcc0519@naver.com>
2023-12-10 14:59:24 +0900

    change name

--- 3-link_RRR_Image.py
+++ 3-link_RRR_Image.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt, animation
 import math
 
 
-def update(frame, Route, link_l1, link_l2, link_l3):
+def update(frame, Route, link_l1_loc, link_l2_loc, link_l3_loc):
     start_x = 15
     start_y = -10
 
@@ -11,16 +11,17 @@ def update(frame, Route, link_l1, link_l2, link_l3):
 
     link_l1_x = [0, x_l1_end[frame]]
     link_l1_y = [0, y_l1_end[frame]]
-    link_l1.set_data(link_l1_x, link_l1_y)
+    link_l1_loc.set_data(link_l1_x, link_l1_y)
+
     link_l2_x = [x_l1_end[frame], x_l2_end[frame]]
     link_l2_y = [y_l1_end[frame], y_l2_end[frame]]
-    link_l2.set_data(link_l2_x, link_l2_y)
+    link_l2_loc.set_data(link_l2_x, link_l2_y)
 
     link_l3_x = [x_l2_end[frame], x_l3_end[frame]]
     link_l3_y = [y_l2_end[frame], y_l3_end[frame]]
-    link_l3.set_data(link_l3_x, link_l3_y)
+    link_l3_loc.set_data(link_l3_x, link_l3_y)
 
-    return Route, link_l1, link_l2, link_l3
+    return Route, link_l1_loc, link_l2_loc, link_l3_loc
 
 
 def make_trajectory(start_x, start_y, end_x, end_y):
@@ -30,6 +31,11 @@ def make_trajectory(start_x, start_y, end_x, end_y):
     y_l2_end_local = []
     x_l3_end_local = []
     y_l3_end_local = []
+
+    loc_theta1_list = []
+    loc_theta2_list = []
+    loc_theta3_list = []
+
     x_trajectory = linspace(start_x, end_x, Route_point)
     y_trajectory = linspace(start_y, end_y, Route_point)
 
@@ -48,6 +54,10 @@ def make_trajectory(start_x, start_y, end_x, end_y):
         theta_2 = -theta_2
         theta_3 = -theta_3
 
+        loc_theta1_list.append(theta_1)
+        loc_theta2_list.append(theta_2)
+        loc_theta3_list.append(theta_3)
+
         x_l1_end_local.append(l1 * cos(theta_1))
         y_l1_end_local.append(l1 * sin(theta_1))
         x_l2_end_local.append(l1 * cos(theta_1) + l2 * (cos(theta_1 + theta_2)))
@@ -55,7 +65,35 @@ def make_trajectory(start_x, start_y, end_x, end_y):
         x_l3_end_local.append(l1 * cos(theta_1) + l2 * (cos(theta_1 + theta_2)) + l3 * cos(theta_1 + theta_2 + theta_3))
         y_l3_end_local.append(l1 * sin(theta_1) + l2 * (sin(theta_1 + theta_2)) + l3 * sin(theta_1 + theta_2 + theta_3))
 
-    return x_l1_end_local, x_l2_end_local, x_l3_end_local, y_l1_end_local, y_l2_end_local, y_l3_end_local
+    return x_l1_end_local, x_l2_end_local, x_l3_end_local, y_l1_end_local, y_l2_end_local, y_l3_end_local, \
+           loc_theta1_list, loc_theta2_list, loc_theta3_list
+
+
+def torque_jacobian(theta_1, theta_2, theta_3):
+    loc_l1 = 10
+    loc_l2 = 8
+    loc_l3 = 6
+    J11 = -loc_l1 * sin(theta_1) - loc_l2 * sin(theta_1 + theta_2) - loc_l3 * sin(theta_1 + theta_2 + theta_3)
+    J12 = -loc_l2 * sin(theta_1 + theta_2) - loc_l3 * sin(theta_1 + theta_2 + theta_3)
+    J13 = -l3 * sin(theta_1 + theta_2 + theta_3)
+
+    J21 = loc_l1 * cos(theta_1) + loc_l2 * cos(theta_1 + theta_2) + loc_l3 * cos(theta_1 + theta_2 + theta_3)
+    J22 = loc_l2 * cos(theta_1 + theta_2) + loc_l3 * cos(theta_1 + theta_2 + theta_3)
+    J23 = loc_l3 * cos(theta_1 + theta_2 + theta_3)
+
+    J = array([[J11, J12, J13],
+               [J21, J22, J23]])
+
+    F = array([[1.0], [1.0]])
+
+    J_T = J.T
+
+    torque = dot(J_T, F)
+    loc_theta1_torque = torque[0]
+    loc_theta2_torque = torque[1]
+    loc_theta3_torque = torque[2]
+
+    return torque
 
 
 x_l1_end = []
@@ -64,38 +102,51 @@ x_l2_end = []
 y_l2_end = []
 x_l3_end = []
 y_l3_end = []
+
 theta1_list = []
 theta2_list = []
 theta3_list = []
+theta1_torque_list = []
+theta2_torque_list = []
+theta3_torque_list = []
 
 first_x = 15
 first_y = -10
 
-final_x = 15
+final_x = 12
 final_y = 0
 
-# Length of links in cm
 l1 = 10
 l2 = 8
 l3 = 6
 
 Route_point = 100
-# Desired Position of End effector
-
 
 phi = 30
 phi = deg2rad(phi)
 
-x_l1_end, x_l2_end,x_l3_end,y_l1_end,y_l2_end,y_l3_end=make_trajectory(first_x, first_y, final_x, final_y)
+x_l1_end, x_l2_end, x_l3_end, y_l1_end, y_l2_end, y_l3_end, theta1_list, theta2_list, theta3_list = make_trajectory(
+    first_x, first_y,
+    final_x, final_y)
+
+for i in range(len(theta1_list)):
+    Torque = torque_jacobian(theta1_list[i], theta2_list[i], theta3_list[i])
+    theta1_torque_list.append(Torque[0])
+    theta2_torque_list.append(Torque[1])
+    theta3_torque_list.append(Torque[2])
+
 figure, ax = plt.subplots()
 
 plt.xlim(0, 30)
 plt.ylim(0, 30)
-Route, = ax.plot([], [], 'k')
+plt.scatter(12, 0, s=50, c='#0000FF', zorder=1)
+plt.plot([12, 18], [0, 0], color="red", linewidth=3, zorder=0)
+Route, = ax.plot([], [], 'k',lw=0)
 link_l1, = ax.plot([], 'b', lw=2)
 link_l2, = ax.plot([], 'r', lw=2)
 link_l3, = ax.plot([], 'g', lw=2)
 ani = animation.FuncAnimation(figure, update,
-                              fargs=(Route, link_l1, link_l2, link_l3))
+                              fargs=( Route, link_l1, link_l2, link_l3))
 plt.show()
 print('asdf')
+plt.show()
--- trash.py
+++ trash.py
@@ -1,19 +1,47 @@
-import cv2
-import numpy as np
+from numpy import *
 
-img=cv2.imread('dataset/triangle.png',cv2.IMREAD_COLOR)
-(h, w) = img.shape[:2]
-(cX, cY) = (w // 2, h // 2)
+l1 = 10.0
+l2 = 8.0
+l3 = 6.0
 
+I1 = 1
+I2 = 1
+I3 = 1
 
-M1 = cv2.getRotationMatrix2D((cX, cY), 90, 1.0)
-M2 = cv2.getRotationMatrix2D((cX, cY), 45, 1.0)
-M3 = cv2.getRotationMatrix2D((cX, cY), 180, 1.0)
-rotated_90 = cv2.warpAffine(img, M1, (w, h))
-rotated_135 = cv2.warpAffine(rotated_90, M2, (w, h))
-rotated_180 = cv2.warpAffine(img, M3, (w, h))
-cv2.imshow('img',img)
-cv2.imshow('img_90',rotated_90)
-cv2.imshow('img_135',rotated_135)
-cv2.imshow('img_180',rotated_180)
-cv2.waitKey()
+m1 = 1.0
+m2 = 1.0
+m3 = 1.0
+
+theta1 = 30.0
+theta1 = deg2rad(theta1)
+theta2 = 30.0
+theta2 = deg2rad(theta2)
+theta3 = 30.0
+theta3 = deg2rad(theta3)
+
+g = 9.81
+
+g1 = m3 * g * ((l1 * cos(theta1)) + 0.5 * l3 * cos(theta1 + theta2 + theta3) + l2 * cos(theta1 + theta2)) + m2 * g * (
+        l1 * cos(theta1) + 0.5 * l2 * cos(theta1 + theta2)) + 0.5 * m1 * g * l1 * cos(theta1)
+
+g2 = m3 * g * (0.5 * l3 * cos(theta1 + theta2 + theta3) + l2 * cos(theta1 + theta2)) + 0.5 * m2 * g * l2 * cos(
+    theta1 + theta2)
+
+g3 = 0.5 * m3 * g * l3 * cos(theta1 + theta2 + theta3)
+
+M11 = l1 ** 2 * (0.25 * m1 + m2 + m3) + l2 ** 2 * (0.25 * m2 + m3) + 0.25 * l3 ** 2 * m3 + l1 * l2 * cos(theta2) * (
+            m2 + 2 * m3) + l2 * l3 * m3 * cos(theta3) + 0.5 * l1 * l3 * m3 * cos(theta2 + theta3) + I1 + I2 + I3
+M12 = l2 ** 2 * (0.25 * m2 + m3) + 0.25 * l3 ** 2 * m3 + l1 * l2 * cos(theta2) * (m3 + 0.5 * m2) + l2 * l3 * m3 * cos(
+    theta3) + 0.5 * l1 * l3 * m3 * cos(theta2 + theta3) + I2 + I3
+M13 = 0.25 * l3 ** 2 * m3 + 0.5 * l2 * l3 * m3 * cos(theta3) + 0.5 * l1 * l3 * m3 * cos(theta2 + theta3) + I3
+
+M21 = l2 ** 2 * (0.25 * m2 + m3) + 0.25 * l3 ** 2 * m3 + l1 * l2 * cos(theta2) * (m3 + 0.5 * m2) + l2 * l3 * m3 * cos(
+    theta3) + 0.5 * l1 * l3 * m3 * cos(theta2 + theta3) + I2 + I3
+M22 = l2 ** 2 * (0.25 * m2 + m3) + 0.25 * l3 ** 2 * m3 + l2 * l3 * m3 * cos(theta3) + I2 + I3
+M23 = 0.25 * l3 ** 2 * m3 + 0.5 * l2 * l3 * m3 * cos(theta3) + I3
+
+M31 = 0.25 * l3 ** 2 * m3 + 0.5 * l2 * l3 * m3 * cos(theta3) + 0.5 * l1 * l3 * m3 * cos(theta2 + theta3) + I3
+M32 = 0.25 * l3 ** 2 * m3 + 0.5 * l2 * l3 * m3 * cos(theta3) + I3
+M33 = 0.25 * l3 ** 2 * m3 + I3
+
+print(M11, M12, M13)
